fa5e4613f92169f164c247377c7d3095feeb2017
Johni Michels <johni.michels@gmail.com>
2019-04-03 08:37:51 -0300

    Ready for initial commit

---
+++ Source/ContainerExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright © 2018 ceCosmos, Brazil. All rights reserved.
+// Project: ProtoStar
+// Author: Johni Michels
+
+using System.Linq;
+using System;
+using System.ComponentModel.Design;
+using Microsoft.Extensions.DependencyInjection;
+using ProtoStar.Core;
+
+namespace ProtoStar.DependencyInjection
+{
+    public static class ContainerExtensions
+    {
+        public static void SolveDependencies(this IServiceProvider container, object dependent)
+        {
+            var genericIDependent = typeof(IDependent<>);
+            var dependencies = dependent.GetType().
+                GetGenericArgumentsForBaseType(genericIDependent).
+                Select(t => t.First());
+            foreach(var t in dependencies)
+            {
+                var dependencyTypeResolver = genericIDependent.MakeGenericType(t);
+                var targetInjectionProperty = dependencyTypeResolver.GetProperties().First();
+                var containerResolved = container.GetService(t);
+                targetInjectionProperty.SetValue(dependent, containerResolved);
+            }
+        }
+
+        public static void AddService<T>(this IServiceContainer provider, T service)=>
+            provider.AddService(typeof(T), service);
+        
+    }
+}
---
+++ Tests/AssignableFallbackContainer.cs
@@ -0,0 +1,47 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProtoStar.DependencyInjection.Tests
+{
+    public class AssignableFallbackContainerTest
+    {
+        [Fact]
+        public void RegisterServices()
+        {
+            var abstractType = typeof(IList<int>);
+            var concrete = new List<int>();
+            var container = new AssignableFallbackContainer();
+            container.AddService<IList<int>>(concrete);
+            Assert.Equal(concrete, container.GetService(abstractType));
+        }
+
+        [Fact]
+        public void FallbackService()
+        {
+            var concrete = new List<int>();
+            var container = new AssignableFallbackContainer();
+            container.AddService(typeof(IList<int>),concrete);
+            Assert.Equal(concrete,container.GetService(typeof(IEnumerable<int>))); 
+        }
+
+        private class MockedConcreteDependency :
+            IDependent<IEnumerable<int>>
+        {
+            public IEnumerable<int> Dependency { get; set; }
+        }
+
+        [Fact]
+        public void InjectDependencies()
+        {
+            var container = new AssignableFallbackContainer();
+            var service = new List<int>() {1,2,3};
+            container.AddService<IList<int>>(service);
+            var dependent = new MockedConcreteDependency();
+            container.SolveDependencies(dependent);
+            Assert.Equal(service,dependent.Dependency);
+        }
+    }
+}
